2751bf9d35ef4e2b99a3dc4d06a9c293f0b21b1b
sirajhanifa <sirajhanifa786@gmail.com>
2025-05-29 19:39:56 +0530

    changes

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Hanifa');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
